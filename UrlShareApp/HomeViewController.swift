5d58e844639c01b8a3e4d90fd2d3db0dbe8b1936
Muhammed Semih <muhammedsemihekmen@hotmail.com>
2023-05-22 00:07:39 +0300

    finished

--- UrlShareApp/HomeViewController.swift
+++ UrlShareApp/HomeViewController.swift
@@ -7,23 +7,52 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
-
+class HomeViewController: UIViewController,UITextFieldDelegate {
+    
+    @IBOutlet weak var themeSwitchButton: UISwitch!
+    @IBOutlet weak var exaLoremLabel: UILabel!
+    @IBOutlet weak var copyButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var nameTextFieldProgressBar: UIProgressView!
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        nameTextField.delegate = self
+    }
 
-        // Do any additional setup after loading the view.
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let lenght = Float(textField.text?.count ?? 1)/10
+        if lenght < 1 {
+            nameTextFieldProgressBar.setProgress(lenght, animated: true)
+        }else{
+            nameTextFieldProgressBar.setProgress(1.0, animated: true)
+        }
+        return true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func ChangeThemeAction(_ sender: UIButton) {
+        themeSwitchButton.setOn(!themeSwitchButton.isOn, animated: true)
+        themeSwitchOnChanged(themeSwitchButton)
     }
-    */
-
+    
+    
+    @IBAction func themeSwitchOnChanged(_ sender: UISwitch) {
+        self.view.backgroundColor =  self.view.backgroundColor == .white ? .lightGray : .white
+    }
+    
+    
+    @IBAction func copyButtonAction(_ sender: UIButton) {
+        let shareCtrl:UIActivityViewController = UIActivityViewController(activityItems: [exaLoremLabel.text], applicationActivities: nil)
+        present(shareCtrl, animated: true)
+        loadingIndicator.isHidden = false
+        loadingIndicator.startAnimating()
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()+2){
+            self.loadingIndicator.isHidden = true
+            self.loadingIndicator.stopAnimating()
+            self.copyButton.setTitle("Copied", for: UIControl.State.application)
+        }
+    }
 }
